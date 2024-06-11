dc9d860fb3426f2ac5e87bf5ad2191ab7fe93f18
barahona1998MP <marcosplata98@gmail.com>
2024-06-10 20:24:15 -0500

    feat: add search-box functionality

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
+import { CountriesRoutingModule } from './countries.routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ByCountryPageComponent,
+    ByCapitalPageComponent,
+    ByRegionPageComponent,
+    CountryPageComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    CountriesRoutingModule
   ]
 })
 export class CountriesModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 
 
@@ -9,13 +13,21 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
   declarations: [
     AboutPageComponent,
     HomePageComponent,
+    SidebarComponent,
+    ContactPageComponent,
+    SearchBoxComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     AboutPageComponent,
+    ContactPageComponent,
     HomePageComponent,
+    SidebarComponent,
+    SearchBoxComponent
+
   ]
 })
 export class SharedModule { }
